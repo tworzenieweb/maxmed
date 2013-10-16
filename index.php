407bf7b1079df15680c713259277dc47387957a1
Lukasz Adamczewski <tworzenieweb@gmail.com>
2013-10-16 23:13:32 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="pl"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang="pl"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="pl"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="pl"> <!--<![endif]-->
+    <head> 
+        <meta content="text/html; charset=utf-8" http-equiv="Content-Type"> 
+        <title>Max Med - w służbie Twojego zdrowia.</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width">
+        <meta name="description" content="Max Med usługi medyczne w służbie Twojego zdrowia.">
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/local.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+        <p class="chromeframe">Korzystasz z bardzo <strong>starej</strong> wersji przeglądarki. 
+        Proszę zaktualizuj <a href="http://browsehappy.com/">swoją przeglądarkę</a> lub
+        <a href="http://www.google.com/chromeframe/?redirect=true">użyj Google Chrome Frame</a> żeby poprawić wyświetlanie strony.</p>
+       <![endif]-->
+
+        <div class="container" id="wrapper">
+
+            <header>
+
+                <div class="clearfix">
+                    <div class="logo pull-left">
+                        <h1>Max Med</h1>
+                        <img src="images/loader.gif" data-src="images/logo.png" alt="Max Med" width="249" height="155" />
+                    </div>
+
+                    <nav class="pull-right">
+                        <ul class="inline bbq-nav bbq-nav-top" id="menu">
+                            <li><a href="#o-firmie.html">O firmie</a></li>
+                            <li><a href="#uslugi.html">Usługi</a></li>
+                            <li><a href="#wspolpraca.html">Współpraca</a></li>
+                            <li><a href="#cennik.html">Cennik</a></li>
+                        </ul>
+
+                    </nav>
+                </div>
+                <div class="banner">
+                    <h2>W służbie Twojego Zdrowia</h2>
+                    <img src="images/loader.gif" data-src="images/banner.jpg" alt="w służbie twojego" width="960" height="248" />
+                </div>
+
+            </header>
+
+            <div class="content clearfix" id="content">
+
+                <div class="left-col pull-left" id="left">
+
+                    <div class="left-col-header clearfix">
+
+                        <div class="col-4">
+                            <h3 class="text-center"><a href="#transport-medyczny.html">Transport Medyczny</a></h3>
+                            <a href="#transport-medyczny.html">
+                                <img src="images/loader.gif" data-src="images/transport.jpg" alt="transport medyczny" />
+                            </a>
+                        </div>
+
+                        <div class="col-4">
+                            <h3 class="text-center"><a href="#zabezpieczenie-imprez.html">Zabezpieczenie Imprez</a></h3>
+                            <a href="#zabezpieczenie-imprez.html">
+                                <img src="images/loader.gif" data-src="images/imprezy.jpg" alt="zabezpieczenie imprez" />
+                            </a>
+                        </div>
+
+                        <div class="col-4">
+                            <h3 class="text-center"><a href="#oferta-zoz.html">Oferta dla ZOZ</a></h3>
+                            <a href="#oferta-zoz.html">
+                                <img src="images/loader.gif" data-src="images/zoz.jpg" alt="oferta dla ZOZ" />
+                            </a>
+                        </div>
+
+                    </div>
+
+                    <div class="content-body">
+
+                        <div class="content-body-inner">
+
+                            <div class="bbq-loading" style="display:none;">
+                                <img src="/images/ajaxload-15-white.gif" alt="Loading"/> Wczytuję...
+                            </div>
+                            
+                            <div class="bbq-default bbq-item">
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="right-col pull-right" id="right">
+
+                    <div class="right-col-inner">
+
+                        <img src="images/loader.gif" data-src="images/ambulans.jpg" alt="ambulans" width="170" height="132" />
+
+                        <h3 class="small-margin"><big>512 550 666</big></h3>
+                        <h3 class="text-center small-margin">Zamów transport medyczny</h3>
+
+                        <section class="contact">
+                            <h4>KONTAKT:</h4>
+
+                            <p><strong>MaxMed</strong><br />
+                                93-578 Łódź<br />
+                                ul. Wróblewskiego 19a lok. 9</p>
+
+                            <p>
+                                NIP: 6580004689<br />
+                                REGON: <br />
+                            </p>
+
+                            <p>e-mail: <a href="mailto:biuro@max-med.eu">biuro@max-med.eu</a></p>
+
+                        </section>
+
+                    </div>
+
+                </div>
+
+
+            </div>
+
+        </div>
+
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+        <script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
+
+        <script src="js/jquery.unveil.min.js"></script>
+        <script src="js/jquery.ba-bbq.min.js"></script>
+        <script src="js/jquery.localScroll.min.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
---
+++ js/plugins.js
@@ -0,0 +1,40 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+$.fn.scrollTo = function( target, options, callback ){
+  if(typeof options == 'function' && arguments.length == 2){ callback = options; options = target; }
+  var settings = $.extend({
+    scrollTarget  : target,
+    offsetTop     : 0,
+    duration      : 500,
+    easing        : 'swing'
+  }, options);
+  return this.each(function(){
+    var scrollPane = $(this);
+    var scrollTarget = (typeof settings.scrollTarget == "number") ? settings.scrollTarget : $(settings.scrollTarget);
+    var scrollY = (typeof scrollTarget == "number") ? scrollTarget : scrollTarget.offset().top + scrollPane.scrollTop() - parseInt(settings.offsetTop);
+    scrollPane.animate({scrollTop : scrollY }, parseInt(settings.duration), settings.easing, function(){
+      if (typeof callback == 'function') { callback.call(this); }
+    });
+  });
+}
